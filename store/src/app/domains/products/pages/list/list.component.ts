9c62fdd759f46b115a52cd73e08d5deb5f6abcff
LuisHZapata <zapataluish@gmail.com>
2024-03-28 11:29:13 -0500

    Buy cart in the store

--- store/src/app/domains/products/pages/list/list.component.ts
+++ store/src/app/domains/products/pages/list/list.component.ts
@@ -2,16 +2,18 @@ import { Component, signal } from '@angular/core';
 import {ProductComponent} from './../../components/product/product.component'
 import { CommonModule } from '@angular/common';
 import {Product} from './../../../shared/models/product.model';
+import {HeaderComponent} from './../../../shared/components/header/header.component'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  cart = signal<Product[]>([]);
 
   constructor(){
     const initProducts: Product[] = [
@@ -35,13 +37,33 @@ export class ListComponent {
         price: 100,
         image: 'https://picsum.photos/640/640?r=26',
         creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=34',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=35',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=36',
+        creationAt: new Date().toISOString()
       }
     ]
     this.products.set(initProducts);
   }
 
-  fromChild(event: string){
-    console.log('Este es un evento del padre');
-    console.log(event);
-  }
+  addToCart(product: Product){
+    this.cart.update(prevState => [...prevState, product])
+}
 }
