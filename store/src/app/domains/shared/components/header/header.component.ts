a4e79b1576f5194554e492fda296fcff0b03690c
LuisHZapata <zapataluish@gmail.com>
2024-04-02 20:13:15 -0500

    Fixing Components

--- store/src/app/domains/shared/components/header/header.component.ts
+++ store/src/app/domains/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Inject, Input, SimpleChanges, signal } from '@angular/core';
 import { Product } from '../../models/product.model';
-import {CommonModule} from '@angular/common'
+import {CommonModule} from '@angular/common';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CartService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -13,8 +14,12 @@ export class HeaderComponent {
   hideSideMenu = signal(true);
 
   @Input({required: true}) cart: Product[] = [];
+  private cartService = Inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.cart;
 
   toogleSideMenu(){
     this.hideSideMenu.update(prevState => !prevState);
   }
+
 }
